ae1f82c7b0f03ea802b603ede6fca9a6dd547184
Alex <afatyga@bu.edu>
2020-04-01 21:04:00 -0400

    worked on web app and added select for time

--- searchTerms.py
+++ searchTerms.py
@@ -3,9 +3,12 @@ import tweepy #twitter api
 from threading import Thread #threading stuff
 from geopy.geocoders import Nominatim
 
-numTweets = 1000
+numTweets = 30
+import datetime
+# datetime object containing current date and time
 
-def getMsgs(searchTerm):
+
+def getMsgs(searchTerm, time):
 	if not isinstance(searchTerm,str): #can only take in a string
 		return []
 
@@ -26,28 +29,39 @@ def getMsgs(searchTerm):
 	api = tweepy.API(auth)
 	listOfLinks = []
 
+	newdate = datetime.datetime.now()
+	if (time == 1): newdate = datetime.datetime.now() - datetime.timedelta(days=1)
+	if (time == 30): newdate = datetime.datetime.now() - datetime.timedelta(days=30)
+	if (time == 365): newdate = datetime.datetime.now() - datetime.timedelta(days=365)
+
 	try:	#will be an error if the username is not valid
 
-		for status in tweepy.Cursor(api.search, q=searchTerm).items(10): 
+		for status in tweepy.Cursor(api.search, q=searchTerm).items(numTweets): 
+#			print(now)
+#			print(status.created_at)
+#			print(abs(status.created_at - now))
 
-			loc = str(status._json['user']['location'])
-			try:
-				geolocator = Nominatim(user_agent="Data Mining")
-				location = geolocator.geocode(loc)
+
+			if (status.created_at > newdate):
+				loc = str(status._json['user']['location'])
+		#	listOfLinks.append([status.text, ])
+				try:
+					geolocator = Nominatim(user_agent="Data Mining1")
+					location = geolocator.geocode(loc)
 				
-				statusLocList = [status.text, location.latitude,location.longitude]
-				listOfLinks.append(statusLocList)
+					statusLocList = [status.text, location.latitude,location.longitude]
+					listOfLinks.append(statusLocList)
 
-			except(AttributeError):
-				pass
+				except(AttributeError):
+					pass
 
+		print(len(listOfLinks))
 		return listOfLinks # a success
 	except (tweepy.TweepError):
 		return [] #means the username was not valid!
 
-def startUp(searchTerm): 
+def startUp(searchTerm, time): 
 
-	geoLocList = getMsgs(searchTerm) 
+	geoLocList = getMsgs(searchTerm, time) 
 	return geoLocList
 
-startUp("dog")
