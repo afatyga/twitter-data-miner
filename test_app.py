6df234616c34c298b6d1ce7e427180a7cb09b601
Noah Malhi <33497238+NoahMalhi@users.noreply.github.com>
2020-05-01 15:49:33 -0400

    adding tests

--- test_app.py
+++ test_app.py
@@ -1,8 +1,14 @@
 import searchTerms
 
-def test_all():
-	assert 1 == 1
+def test_getmsg():
+    assert searchTerms.getMsgs("covid", 1) == [[3, 40.7127281, -74.0060152, 'New York City, NY'], [3, 40.7127281, -74.0060152, 'New York City, NY']]
+[40.7127281, -74.0060152, 40.7127281, -74.0060152]]
+    
+def test_sent():
+    assert searchTerms.get_tweet_sentiment("terrible, I despise today") == 3
+    assert searchTerms.get_tweet_sentiment("goood, I reaLLY LIKE CHOCOLATE CAKE") == 1
+    assert searchTerms.get_tweet_sentiment("@happy you are really dumb") == 3
 
+def remove_noise():
+    assert searchTerms.remove_noise("@Trump is really dumb") == "Trump is really dumb"
 
-if __name__ == "__main__":
-    test_all()
