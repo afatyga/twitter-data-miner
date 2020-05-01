218546a22d5090e545c033db0c465bfa671f61b5
Noah Malhi <33497238+NoahMalhi@users.noreply.github.com>
2020-05-01 16:17:52 -0400

    tests working

--- test_app.py
+++ test_app.py
@@ -4,14 +4,15 @@ import searchTerms
 searchTerms.calibrate()
 
 def test_getmsg():
-    assert searchTerms.getMsgs("covid", 1) == [[3, 40.7127281, -74.0060152, 'New York City, NY'], [3, 40.7127281, -74.0060152, 'New York City, NY']
-[40.7127281, -74.0060152, 40.7127281, -74.0060152]]
-    
-def test_sent():
-    assert searchTerms.get_tweet_sentiment("terrible, I despise today") == 3
-    assert searchTerms.get_tweet_sentiment("goood, I reaLLY LIKE CHOCOLATE CAKE") == 1
-    assert searchTerms.get_tweet_sentiment("@happy you are really dumb") == 3
+    assert searchTerms.getMsgs("covid", 1) == [[3, 40.7127281, -74.0060152, 'New York City, NY'], [1, 40.7127281, -74.0060152, 'New York City, NY']]
 
 def remove_noise():
     assert searchTerms.remove_noise("@Trump is really dumb") == "is really dumb"
+        
+def test_sent():
+    assert searchTerms.get_tweet_sentiment("terrible, I despise today") == 3
+    assert searchTerms.get_tweet_sentiment("i am having a good day!") == 1
+  
+
+
 
