6fcbcf950462a1eeee3d841bc662c12896894ccf
Noah Malhi <33497238+NoahMalhi@users.noreply.github.com>
2020-05-01 16:21:32 -0400

    added comments to tests

--- test_app.py
+++ test_app.py
@@ -4,15 +4,15 @@ import searchTerms
 searchTerms.calibrate()
 
 def test_getmsg():
-    assert searchTerms.getMsgs("covid", 1) == [[3, 40.7127281, -74.0060152, 'New York City, NY'], [1, 40.7127281, -74.0060152, 'New York City, NY']]
+    assert searchTerms.getMsgs("covid", 1) == [[3, 40.7127281, -74.0060152, 'New York City, NY'], [3, 40.7127281, -74.0060152, 'New York City, NY']] #tests using jsons file of tweets
 
 def remove_noise():
-    assert searchTerms.remove_noise("@Trump is really dumb") == "is really dumb"
-        
+    assert searchTerms.remove_noise("@Trump is really dumb") == "is really dumb"    #able to parse out #
+
 def test_sent():
-    assert searchTerms.get_tweet_sentiment("terrible, I despise today") == 3
-    assert searchTerms.get_tweet_sentiment("i am having a good day!") == 1
-  
+    assert searchTerms.get_tweet_sentiment("terrible, I despise today") == 3        #test negative tweet
+    assert searchTerms.get_tweet_sentiment("i am having a good day!") == 1          #test positive tweet
+    
 
 
 
